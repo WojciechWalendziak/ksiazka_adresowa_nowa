63643c45bb0ae12d50af18996360232794714156
gwadelupa72 <walendziak92@wp.pl>
2020-04-30 21:51:27 +0200

    Load user to vector on start program

---
+++ Ksiazkaadresowa.cpp
@@ -0,0 +1,15 @@
+#include "KsiazkaAdresowa.h"
+
+KsiazkaAdresowa::KsiazkaAdresowa()
+{
+    uzytkownikMenedzer.wczytajUzytkownikowZPliku();
+}
+void KsiazkaAdresowa::rejestracjaUzytkownika()
+{
+    uzytkownikMenedzer.rejestracjaUzytkownika();
+}
+
+void KsiazkaAdresowa::wypiszWszystkichUzytkownikow()
+{
+    uzytkownikMenedzer.wypiszWszystkichUzytkownikow();
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Ksiazkaadresowa.h"
 
+using namespace std;
+
+int main()
+{
+    KsiazkaAdresowa ksiazkaAdresowa;
+    //ksiazkaAdresowa.wczytajUzytkownikowZPliku();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+
+    return 0;
+}
